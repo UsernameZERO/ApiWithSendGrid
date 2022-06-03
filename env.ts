ab34f86d65b832bf9659bb5cef174af2f3d9c1a6
Zero <iamvamsi999@gmail.com>
2022-06-03 18:32:17 +0530

    commit

--- env.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    SENDGRID_API_KEY : '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',
-    Url: 'mongodb+srv://UsernameZero:UsernameZero@cluster0.oe1eg.mongodb.net/ApiTestingMail'
-}
